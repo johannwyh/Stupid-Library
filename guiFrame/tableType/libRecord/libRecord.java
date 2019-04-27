80821ad77eb22e1046c41d751966822ab6494def
unispac <q416162623@gmail.com>
2019-04-27 21:17:15 +0800

    debug

--- guiFrame/tableType/libRecord/libRecord.java
+++ guiFrame/tableType/libRecord/libRecord.java
@@ -12,4 +12,28 @@ public class libRecord
         this.dateReturn=new SimpleStringProperty(dateReturn);
         this.supervisior=new SimpleStringProperty(supervisior);
     }
+    public String getCardId()
+    {
+        return this.cardId.get();
+    }
+    public String getBookId()
+    {
+        return this.bookId.get();
+    }
+    public String getDateBorrow()
+    {
+        return this.dateBorrow.get();
+    }
+    public String getDateDue()
+    {
+        return this.dateDue.get();
+    }
+    public String getDateReturn()
+    {
+        return this.dateReturn.get();
+    }
+    public String getSupervisior()
+    {
+        return this.supervisior.get();
+    }
 }
